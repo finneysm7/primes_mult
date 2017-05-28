0f9f5385ef5fc29800ca3812a153344f31cfc2d5
Sean Finney <mcfinneys@gmail.com>
2017-05-28 16:54:05 -0700

    add seive unit tests

--- unit_test.py
+++ unit_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from mult_table import find_primes
+from mult_table import find_primes, seive_of_eratosthenes
 
 class TestFindPrimes(unittest.TestCase):
     def test_returns_primes_norm(self):
@@ -27,6 +27,17 @@ class TestFindPrimes(unittest.TestCase):
         expected = [2,3]
         actual = find_primes(2)
         self.assertEqual(expected, actual)
+
+class TestSieve(unittest.TestCase):
+    def test_returns_primes(self):
+        expected = [True, True, True, True, False, True, False, True, False, False, False]
+        actual = seive_of_eratosthenes(10)
+        self.assertEqual(expected,actual)
+
+    def test_does_not_return_invalid_primes(self):
+        expected = [True,True,True,True,True,True,True]
+        actual = seive_of_eratosthenes(6)
+        self.assertNotEqual(expected,actual)
 
 if __name__ == '__main__':
     unittest.main()
